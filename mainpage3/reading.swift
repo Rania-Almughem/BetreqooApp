4926a806e18ca4a2f32b1a88c1ca8cf5e270e7d8
Norashafiq <noura.shafiq@gmail.com>
2023-10-16 23:08:09 +0300

    m

---
+++ mainpage3/reading.swift
@@ -0,0 +1,18 @@
+//
+//  reading.swift
+//  mainpage3
+//
+//  Created by Noura Almutahar on 01/04/1445 AH.
+//
+
+import SwiftUI
+
+struct reading: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    reading()
+}
