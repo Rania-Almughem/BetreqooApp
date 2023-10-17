fb08dcfe36e918ef6da8bddb4a45bf3ef03cfcbe
Majd <llmajdll22@gmail.com>
2023-10-17 10:12:51 +0300

    majd work

---
+++ mainpage3/SplashscreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashscreenView.swift
+//  mainpage3
+//
+//  Created by Majd Abdullah on 02/04/1445 AH.
+//
+
+import SwiftUI
+
+struct SplashscreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashscreenView()
+}
