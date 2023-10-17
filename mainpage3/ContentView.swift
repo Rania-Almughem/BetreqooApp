0354e87d60486d162b08817319a510d58afe3e55
Majd <llmajdll22@gmail.com>
2023-10-17 10:13:20 +0300

    majd2

--- mainpage3/ContentView.swift
+++ mainpage3/ContentView.swift
@@ -4,20 +4,66 @@
 //
 //  Created by rania on 29/03/1445 AH.
 //
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("mjd")
+
+    struct ContentView: View {
+        @State private var moveTextUp = false
+
+        var body: some View {
+            ZStack {
+                Color.color
+                    .edgesIgnoringSafeArea(.all)
+
+                VStack {
+                    Image("ice")
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: 1000, height: 400)
+                        .padding(.top, 1000)
+                }
+
+                VStack {
+                    Image("pe")
+                        .resizable()
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: 200, height: 200)
+                        .padding(.top, 1000)
+                }
+
+                VStack {
+                    Text("هيّــا بنـا نتعــلم مــع")
+                        .font(.custom("Ithra", size: 65))
+                        .bold()
+                        .foregroundColor(Color.color1)
+                        .frame(alignment: .center)
+                        .kerning(3)
+                        .offset(y: moveTextUp ? -20 : 0) // Move text up by 20 points
+                        .animation(
+                            Animation.easeInOut(duration: 1) // Animation duration and timing curve
+                                .repeatForever(autoreverses: true) // Repeats animation indefinitely
+                        )
+
+                    Text("بــطريــقو !")
+                        .font(.custom("Ithra", size: 65))
+                        .bold()
+                        .foregroundColor(Color.color2)
+                        .frame(alignment: .center)
+                        .kerning(3)
+                        .offset(y: moveTextUp ? 0 : 20) // Move text down by 20 points
+                        .animation(
+                            Animation.easeInOut(duration: 1)
+                                .repeatForever(autoreverses: true)
+                        )
+                }
+            }
+            .onAppear {
+                // Start the animation when the view appears
+                self.moveTextUp.toggle()
+            }
         }
-        .padding()
     }
-}
+
 
 #Preview {
     ContentView()
--- mainpage3/SplashscreenView.swift
+++ mainpage3/SplashscreenView.swift
@@ -5,14 +5,47 @@
 //  Created by Majd Abdullah on 02/04/1445 AH.
 //
 
+
 import SwiftUI
 
 struct SplashscreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isActive {
+            ContentView()
+        } else {
+            
+            VStack{
+                
+                    Color.color
+                        .edgesIgnoringSafeArea(.all)
+      
+                .opacity(opacity)
+                .onAppear{
+                    withAnimation(.easeIn(duration: 1.0)){
+                        self.size = 0.5
+                        self.opacity = 1
+                    }
+                }
+            }
+            
+            
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+                    withAnimation{
+                        self.isActive = true
+                    }
+                    
+                }
+            }
+        }
     }
 }
 
 #Preview {
     SplashscreenView()
 }
+
